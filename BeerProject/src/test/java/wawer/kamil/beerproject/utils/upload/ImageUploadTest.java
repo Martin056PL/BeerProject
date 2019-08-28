2982f7f138260dc76933221ad3fe5837f998f025
Kamil Wawer <wawerk@tt.com.pl>
2019-08-28 13:02:09 +0200

    Fix tests names

--- BeerProject/src/test/java/wawer/kamil/beerproject/utils/upload/ImageUploadTest.java
+++ BeerProject/src/test/java/wawer/kamil/beerproject/utils/upload/ImageUploadTest.java
@@ -24,12 +24,12 @@ public class ImageUploadTest {
     ImageUpload upload;
 
     private static final String STANDARD_TYPE = "image/jpeg";;
-    private static final long ACCEPTSIZE = 10L;
-    private static final long WRONGSIZE = 1000000000L;
+    private static final long ACCEPT_SIZE = 10L;
+    private static final long WRONG_SIZE = 1000000000L;
     private static final String BAD_TYPE = "CONTEXT";
 
     @Test
-    public void asd() throws IOException {
+    public void should_return_true_when_controller_returns_the_same_length_of_byte_array_as_byte_array_from_file() throws IOException {
         when(file.getBytes()).thenReturn(newArray());
         byte [] bytes = newArrayWithLength(file.getBytes().length);
         int i = 0;
@@ -40,29 +40,29 @@ public class ImageUploadTest {
     }
 
     @Test
-    public void good_good(){
-        when(file.getSize()).thenReturn(ACCEPTSIZE);
+    public void should_return_true_if_size_and_type_are_correct(){
+        when(file.getSize()).thenReturn(ACCEPT_SIZE);
         when(file.getContentType()).thenReturn(STANDARD_TYPE);
         assertTrue(upload.validateSizeAndTypeOfFile(file));
     }
 
     @Test
-    public void good_bad(){
-        when(file.getSize()).thenReturn(ACCEPTSIZE);
+    public void should_return_false_if_size_are_correct_and_type_are_incorrect(){
+        when(file.getSize()).thenReturn(ACCEPT_SIZE);
         when(file.getContentType()).thenReturn(BAD_TYPE);
         assertFalse(upload.validateSizeAndTypeOfFile(file));
     }
 
     @Test
-    public void bad_good(){
-        when(file.getSize()).thenReturn(WRONGSIZE);
+    public void should_return_false_if_size_are_incorrect_and_type_is_correct(){
+        when(file.getSize()).thenReturn(WRONG_SIZE);
         when(file.getContentType()).thenReturn(STANDARD_TYPE);
         assertFalse(upload.validateSizeAndTypeOfFile(file));
     }
 
     @Test
-    public void bad_bad(){
-        when(file.getSize()).thenReturn(WRONGSIZE);
+    public void should_return_false_if_size_and_type_are_incorrect(){
+        when(file.getSize()).thenReturn(WRONG_SIZE);
         when(file.getContentType()).thenReturn(BAD_TYPE);
         assertFalse(upload.validateSizeAndTypeOfFile(file));
     }
