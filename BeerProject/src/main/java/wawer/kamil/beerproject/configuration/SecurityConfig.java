a6c54079093a2502c5b6f38a02553d132e98d010
Kamil Wawer <kamil.wawer@pollub.edu.pl>
2019-10-01 21:54:06 +0200

    Fix problem with h2-console, switch on liquibase

--- BeerProject/src/main/java/wawer/kamil/beerproject/configuration/SecurityConfig.java
+++ BeerProject/src/main/java/wawer/kamil/beerproject/configuration/SecurityConfig.java
@@ -58,28 +58,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors()
-                .and()
+
                 .csrf()
                 .disable()
+                .cors()
+                .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .headers()
+                .frameOptions()
+                .disable()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/",
-                        "/favicon.ico",
-                        "/**/*.png",
-                        "/**/*.gif",
-                        "/**/*.svg",
-                        "/**/*.jpg",
-                        "/**/*.html",
-                        "/**/*.css",
-                        "/**/*.js")
-                .permitAll()
-                .antMatchers("/auth/**")
+                .antMatchers("/auth/**", "/h2-console/**")
                 .permitAll()
                 .antMatchers("/user/checkUsernameAvailability", "/user/checkEmailAvailability")
                 .permitAll()
