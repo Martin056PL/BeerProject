78b703101aee4adea4360409f471337282754c29
Kamil <kamil.wawer@pollub.edu.pl>
2022-03-13 16:57:47 +0100

    Bump versions

--- BeerProject/src/main/java/wawer/kamil/beerproject/aspect/controller/BeerAspects.java
+++ BeerProject/src/main/java/wawer/kamil/beerproject/aspect/controller/BeerAspects.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+import wawer.kamil.beerproject.dto.BeerDTO;
 import wawer.kamil.beerproject.dto.request.BeerRequest;
 
 @Aspect
