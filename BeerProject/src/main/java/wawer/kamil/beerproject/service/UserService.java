b56d009c6272218d156b3dd432e145c2611b0ade
Kamil <kamil.wawer@pollub.edu.pl>
2022-03-24 18:46:56 +0100

    remove unused imports

--- BeerProject/src/main/java/wawer/kamil/beerproject/service/UserService.java
+++ BeerProject/src/main/java/wawer/kamil/beerproject/service/UserService.java
@@ -4,8 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import wawer.kamil.beerproject.dto.request.UserRequest;
 import wawer.kamil.beerproject.dto.response.UserResponse;
-import wawer.kamil.beerproject.exceptions.ElementNotFoundException;
-import wawer.kamil.beerproject.exceptions.UsernameAlreadyExistsException;
 import wawer.kamil.beerproject.model.User;
 
 import java.util.List;
@@ -18,11 +16,11 @@ public interface UserService {
 
     List<UserResponse> findAllUsersList();
 
-    UserResponse findUserByUserId(Long userId) ;
+    UserResponse findUserByUserId(Long userId);
 
     UserResponse addNewUser(UserRequest userRequest);
 
-    UserResponse updateUser(Long userId, UserRequest userRequest) ;
+    UserResponse updateUser(Long userId, UserRequest userRequest);
 
-    void permanentDeleteUser(Long userId) ;
+    void permanentDeleteUser(Long userId);
 }
