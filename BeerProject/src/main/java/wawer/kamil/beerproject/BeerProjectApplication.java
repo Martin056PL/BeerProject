10ea1c5fa4eafeca4296539d6c4f566a48c6594a
Kamil <kamil.wawer@pollub.edu.pl>
2022-03-26 14:46:46 +0100

    Remove Jasypt

--- BeerProject/src/main/java/wawer/kamil/beerproject/BeerProjectApplication.java
+++ BeerProject/src/main/java/wawer/kamil/beerproject/BeerProjectApplication.java
@@ -1,13 +1,11 @@
 package wawer.kamil.beerproject;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-@EnableEncryptableProperties
 public class BeerProjectApplication {
 
     public static void main(String[] args) {
